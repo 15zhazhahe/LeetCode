fb31449fa78c2c4e329d1b7093d3402be84083ae
15zhazhahe <729260486@qq.com>
2018-01-26 15:09:03 +0800

    657. Judge Route Circle

---
+++ Python/657. Judge Route Circle.py
@@ -0,0 +1,22 @@
+# Time:  O(n)
+# Space: O(1)
+class Solution:
+    def judgeCircle(self, moves):
+        """
+        :type moves: str
+        :rtype: bool
+        """
+        x = y = 0
+        for move in moves:
+            if move == 'U':
+                y += 1
+            elif move == 'D':
+                y -= 1
+            elif move == 'L':
+                x -= 1
+            else:
+                x += 1
+        if x == 0 and y == 0:
+            return True
+        else:
+            return False

ea515fc50b3166f09f62c46897830b2fc59cf364
15zhazhahe <729260486@qq.com>
2018-01-31 21:25:11 +0800

    766. Toeplitz Matrix

---
+++ C++/766. Toeplitz Matrix.cpp
@@ -0,0 +1,16 @@
+// Time: O(n*m)
+class Solution {
+public:
+    bool isToeplitzMatrix(vector<vector<int>>& matrix) {
+        int n = matrix.size(),m = matrix[0].size();
+        for(int i=0;i<n-1;i++)
+        {
+            for(int j=0;j<m-1;j++)
+            {
+                if(matrix[i][j] != matrix[i+1][j+1])
+                    return false;
+            }
+        }
+        return true;
+    }
+};

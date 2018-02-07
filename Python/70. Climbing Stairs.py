3a3848041d4df1fa02ddd5a466ad0b4cdf90d9ae
15zhazhahe <729260486@qq.com>
2018-02-08 01:01:19 +0800

    70. Climbing Stairs

---
+++ Python/70. Climbing Stairs.py
@@ -0,0 +1,12 @@
+class Solution:
+    def climbStairs(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        dp = list()
+        dp.append(1)
+        dp.append(1)
+        for i in range(2,n+1):
+            dp.append(dp[i-1]+dp[i-2])
+        return dp[n]

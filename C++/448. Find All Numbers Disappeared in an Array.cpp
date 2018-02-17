f282beed5df53957417b4df441463d726276f93c
15zhazhahe <729260486@qq.com>
2018-02-17 12:08:23 +0800

    448. Find All Numbers Disappeared in an Array

---
+++ C++/448. Find All Numbers Disappeared in an Array.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    vector<int> findDisappearedNumbers(vector<int>& nums) {
+        vector<int> ans;
+        for(int i=0;i<nums.size();i++)
+        {
+            int j = abs(nums[i]) - 1;
+            nums[j] = -abs(nums[j]);
+        }
+        for(int i=0;i<nums.size();i++)
+        {
+            if(nums[i]>0)
+                ans.push_back(i+1);
+        }
+        return ans;
+    }
+};

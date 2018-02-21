23d1e2b51c9e92ef6a9ed4111dcaa4c77cbc8f7a
15zhazhahe <729260486@qq.com>
2018-02-21 22:30:20 +0800

    217. Contains Duplicate

---
+++ Python/217. Contains Duplicate.py
@@ -0,0 +1,9 @@
+class Solution:
+    def containsDuplicate(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: bool
+        """
+        s = set(nums)
+        return len(nums) > len(s)
+                

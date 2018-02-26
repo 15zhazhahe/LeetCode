98e21c2b92e974ce49f72a2362676755c77a4642
15zhazhahe <729260486@qq.com>
2018-02-26 12:01:31 +0800

    69. Sqrt(x)

---
+++ Python/69. Sqrt(x).py
@@ -0,0 +1,16 @@
+class Solution:
+    def mySqrt(self, x):
+        """
+        :type x: int
+        :rtype: int
+        """
+        l, r = 0, x
+        while l <= r:
+            mid = l + (r - l) // 2
+            if mid * mid == x:
+                return mid
+            elif mid * mid > x:
+                r = mid - 1
+            elif mid * mid < x:
+                l = mid + 1
+        return l - 1;
